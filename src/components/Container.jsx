f1e2fe9dd4e00835fb70f30a8aebc9c392a4588d
Pierre ANDRESKI <pierre.andreski@gmail.com>
2022-12-13 22:03:54 -0500

    Fix styled-components warnings

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,26 +1,25 @@
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
+const Wrapper = styled.div`
+  padding: 0 20px;
+
+  @media screen and (min-width: 768px) {
+    max-width: 720px;
+    margin: 0 auto;
+    padding: 0;
+  }
+  @media screen and (min-width: 992px) {
+    max-width: 960px;
+  }
+  @media screen and (min-width: 1200px) {
+    max-width: 1140px;
+  }
+  @media screen and (min-width: 1400px) {
+    max-width: 1240px;
+  }
+`;
 export default function Container({ children }) {
-  const Wrapper = styled.div`
-    padding: 0 20px;
-
-    @media screen and (min-width: 768px) {
-      max-width: 720px;
-      margin: 0 auto;
-      padding: 0;
-    }
-    @media screen and (min-width: 992px) {
-      max-width: 960px;
-    }
-    @media screen and (min-width: 1200px) {
-      max-width: 1140px;
-    }
-    @media screen and (min-width: 1400px) {
-      max-width: 1240px;
-    }
-  `;
-
   return (
     <Wrapper>
       {children}
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,52 +3,52 @@ import styled from 'styled-components';
 import DefaultLogo from '../assets/images/logo.svg';
 import colors from '../utils/style/colors';
 
-export default function Header() {
-  const Logo = styled.img`
-    width: 145px;
-    height: auto;
+const Logo = styled.img`
+  width: 145px;
+  height: auto;
 
-    @media screen and (min-width: 992px) {
-      width: 210px;
-    }
-  `;
+  @media screen and (min-width: 992px) {
+    width: 210px;
+  }
+`;
 
-  const Wrapper = styled.header`
+const Wrapper = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding-top: 20px;
+
+  nav ul {
+    list-style-type: none;
     display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding-top: 20px;
+    padding: 0;
+    margin: 0;
 
-    nav ul {
-      list-style-type: none;
-      display: flex;
-      padding: 0;
-      margin: 0;
+    li + li {
+      padding-left: 15px;
 
-      li + li {
-        padding-left: 15px;
-
-        @media screen and (min-width: 768px) {
-          padding-left: 60px;
-        }
+      @media screen and (min-width: 768px) {
+        padding-left: 60px;
       }
     }
-  `;
+  }
+`;
 
-  const MenuLink = styled(NavLink)`
-    font-size: .75em;
-    color: ${colors.primary};
-    text-decoration: none;
+const MenuLink = styled(NavLink)`
+  font-size: .75em;
+  color: ${colors.primary};
+  text-decoration: none;
 
-    &.active {
-      text-decoration: underline;
-    }
+  &.active {
+    text-decoration: underline;
+  }
 
-    @media screen and (min-width: 768px) {
-      font-size: 1.5em;
-    }
-  `;
+  @media screen and (min-width: 768px) {
+    font-size: 1.5em;
+  }
+`;
 
+export default function Header() {
   return (
     <Wrapper>
       <Logo src={DefaultLogo} alt="Logo Kasa" />
